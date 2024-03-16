ae3f00e03ab6b5cffce55de1d31c8a2bb010d3b8
JOUNAL <136606554+JOUNAL@users.noreply.github.com>
2024-03-15 23:29:29 -0500

    Update Menu_class.py

--- Reto_3/Menu_class.py
+++ Reto_3/Menu_class.py
@@ -3,26 +3,29 @@ class Order:
         self.list =[]
 
     def total_price(self):
-        self.total = sum([item.price * item.quantity for item in self.items])
+        self.total = sum([item.subtotal for item in self.list])
         return self.total
 
     def discount(self, discount):
+        self.total = sum([item.subtotal for item in self.list])
         self.total -= self.total * (discount / 100)
         return self.total
 
     def add_item(self, item):
-        self.items.append(item)
+        self.list.append(item)
 
-    def show_order(self):
-        return self.list
+    def get_list(self):
+        print("Nombre                 Precio")
+        for item in self.list:
+            print(f"{item.name} x {item.quantity}    {item.price}")
 
 
 class MenuItem:
-    def __init__(self, name, price,quantity,):
+    def __init__(self, name, price,quantity):
         self.name = name
         self.price = price
         self.quantity = quantity
-
+        self.subtotal= self.price*self.quantity
 
 class Juice(MenuItem):
     def __init__(self, name, price,quantity,sugar):
@@ -73,3 +76,13 @@ class Appetizer(MenuItem):
     def __init__(self, name, price,quantity,kind):
         super().__init__(name, price,quantity)
         self.kind=kind
+
+orden=Order()
+
+orden.add_item(Juice(name="Jugo de mango",price=2000,quantity=3,sugar="No"))
+orden.add_item(Soup(name="Sancocho de pescado",price=4000,quantity=2,kind="Sancocho"))
+orden.add_item(MainDish(name="Cerdo a la llanera",price=9000,quantity=2,description="Cerdo con salsa de la casa, arroz, patacon y ensalada"))
+orden.add_item(Appetizer(name="Parfait",price=3500,quantity=2,kind="Dulce"))
+
+print("El precio total de la orden es:" + str(orden.total_price()))
+orden.get_list()
